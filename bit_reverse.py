55d44ee30147a086fdbdf2ffb22d3aab616d092b
shunak <shuna966@gmail.com>
2019-02-24 22:26:53 +0900

    190224

--- bit_reverse.py
+++ bit_reverse.py
@@ -1,4 +1,4 @@
-## ビット反転演算
+## ビット反転演算(初期状態|0>)
 from qiskit import *
 q = QuantumRegister(1)  # １つの 量子 レジスタqの 生成
 c = ClassicalRegister(1)  # １つの 古典 的 レジスタcの 生成
@@ -11,3 +11,7 @@ backend_sim = BasicAer.get_backend('qasm_simulator')
 r = execute(qc, backend_sim, shots=100).result()
 #量子 回路 名 cnの 量子 プログラムの 実 行 結 果 rからカウント 結 果 取得 し 表示 する
 print(r.get_counts())
+
+# the results
+# inital state '0' turn to '1' by X operator
+# {'1': 100}
