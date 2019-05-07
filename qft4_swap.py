74a51081e253415330bcd586597de00a0d620bcd
shunak <shuna966@gmail.com>
2019-05-08 00:08:39 +0900

    190507

---
+++ qft4_swap.py
@@ -0,0 +1,34 @@
+#Swapゲートによる上位・下位ビットを入れ替えた量子フーリエ変換（QFT4）
+from qiskit import *
+from qiskit.tools.visualization import plot_histogram, circuit_drawer
+import math
+def swap(qci, s1, s2):
+    qci.cx(s1,s2)
+    qci.cx(s2,s1)
+    qci.cx(s1,s2)
+
+bn=2 # 量子ビット数
+
+q = QuantumRegister(bn) #ba個の 量子 レジスタqの 生成
+c = ClassicalRegister(bn) #bn個の 古典 的 レジスタcの 生成
+qc = QuantumCircuit(q, c) #量子回路の生成
+# 量子ゲート部分　＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+
+qc.h(q[0])
+qc.cu1(math.pi/2, q[0], q[1])
+qc.h(q[1])
+swap(qc, q[0],q[1])
+for i in range(bn):
+    qc.measure(q[bn-1-i],c[i])
+#
+#         # 量子ゲート部分　＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+#
+#         #量子 回路 を 実 行 し、 結 果 rに 代入 する
+#         # Use Aer's qasm_simulator
+backend_sim = BasicAer.get_backend('qasm_simulator')
+r = execute(qc, backend_sim, shots=8192).result()
+rc = r.get_counts()
+print(rc)
+#         # circuit_drawer(qc)
+plot_histogram(rc)
+#
